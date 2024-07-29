b634b434e8b9847e51dba442a68f91c4b8462ac5
Nanamoriyama <nanamoriyama@outlook.jp>
2024-07-29 16:21:14 +0200

    changed Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
+
 import NewsLetter from "@/components/NewsLetter";
 import Selection from "@/components/Selection";
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 "use client";
 import Link from "next/link";
 import React, { useState, useRef, useEffect } from "react";
-import { IoIosSend } from "react-icons/io";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
-const NewsLetter = () => {
+const Header = () => {
   const [isAtStart, setIsAtStart] = useState(true);
   const [isAtEnd, setIsAtEnd] = useState(false);
   const scrollRef = useRef(null);
@@ -13,7 +12,7 @@ const NewsLetter = () => {
     if (scrollRef.current) {
       const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
       setIsAtStart(scrollLeft === 0);
-      setIsAtEnd(scrollLeft + clientWidth === scrollWidth);
+      setIsAtEnd(scrollLeft + clientWidth >= scrollWidth - 1);
     }
   };
 
@@ -34,50 +33,54 @@ const NewsLetter = () => {
   };
 
   return (
-    <>
-      <div className="flex flex-col justify-evenly p-4 relative">
-        <div className="font-light text-s md:flex md:justify-between md:items-center relative gradient-overlay">
-          {!isAtStart && (
-            <div className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10">
-              <FaAngleLeft className="" />
-            </div>
-          )}
-          <div
-            className="overflow-x-auto hide-scrollbar relative z-0"
-            ref={scrollRef}
-            onScroll={handleScroll}
-          >
-            <ul className="flex space-x-7 whitespace-nowrap">
-              {[
-                { href: "/woman", label: "Gifts for Her" },
-                { href: "/men", label: "Gifts for Him" },
-                { href: "/bags", label: "Woman's Bags" },
-                { href: "/men", label: "Men's sneakers" },
-                { href: "/jewelry", label: "Woman's Fashion Jewelry" },
-                { href: "/kids", label: "Kids & Baby" },
-                { href: "/maison", label: "Maison" },
-                { href: "/jewelry", label: "For The Home" },
-              ].map((item, index) => (
-                <li key={index} className="relative group">
-                  <Link
-                    href={item.href}
-                    className="underline-left hover:text-stone-400"
-                  >
-                    {item.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+    <div className="flex flex-col justify-evenly p-4 relative">
+      <div className="font-light text-s md:flex md:justify-between md:items-center relative gradient-overlay">
+        {!isAtStart && !isAtEnd && (
+          <div className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10">
+            <FaAngleLeft className="" />
           </div>
-          {!isAtEnd && (
-            <div className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10">
-              <FaAngleRight className="" />
-            </div>
-          )}
+        )}
+        <div
+          className="overflow-x-auto hide-scrollbar relative z-0"
+          ref={scrollRef}
+          onScroll={handleScroll}
+          style={{ overflowY: "hidden" }} // Hide vertical scrollbar
+        >
+          <ul className="flex space-x-7 whitespace-nowrap m-0 p-0">
+            {[
+              { href: "/woman", label: "Gifts for Her" },
+              { href: "/men", label: "Gifts for Him" },
+              { href: "/bags", label: "Woman's Bags" },
+              { href: "/men", label: "Men's sneakers" },
+              { href: "/jewelry", label: "Woman's Fashion Jewelry" },
+              { href: "/kids", label: "Kids & Baby" },
+              { href: "/maison", label: "Maison" },
+              { href: "/maison", label: "For The Home" },
+              { href: "/woman", label: "Woman's Fashion" },
+              { href: "/bags", label: "Lady Dior" },
+            ].map((item, index) => (
+              <li
+                key={index}
+                className={`relative group ${isAtEnd ? "no-gradient" : ""}`}
+              >
+                <Link
+                  href={item.href}
+                  className="underline-left hover:text-stone-400"
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
+        {!isAtEnd && (
+          <div className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10">
+            <FaAngleRight className="" />
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
-export default NewsLetter;
+export default Header;
